630eca87eccf94039d3160f1fb8a6ff94dde8765
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-02-21 11:05:21 +0100

    Add tests

--- tests/test__private.py
+++ tests/test__private.py
@@ -1,5 +1,13 @@
-from airly._private import _DictToObj
+import json
 from unittest import TestCase
+from unittest.mock import AsyncMock, Mock
+
+from aiounittest import AsyncTestCase
+from airly import Airly
+from airly._private import _DictToObj
+
+LATITUDE = 12
+LONGITUDE = 21
 
 class _DictToObjTestCase(TestCase):
     def test_init_with_iterable(self):
@@ -16,3 +24,39 @@ class _DictToObjTestCase(TestCase):
         self.assertEqual('value1', sut.key1)
         self.assertEqual(2, sut['key2'])
         self.assertEqual(2, sut.key2)
+
+
+class HeadersTestCase(AsyncTestCase):
+    async def test_valid_headers(self):
+        with open("data/measurements_typical.json") as file:
+            data = json.load(file)
+        headers = {"X-RateLimit-Limit-day": "1000", "X-RateLimit-Remaining-day": "993"}
+        with Mock(
+            get=AsyncMock(side_effect=data),
+            headers=Mock(side_effect=headers),
+            status=200,
+            __enter__=Mock(),
+            __exit__=Mock(),
+        ) as mock_session:
+            airly = Airly("abcdef", mock_session)
+            measurements = airly.create_measurements_session_point(LATITUDE, LONGITUDE)
+            await measurements.update()
+            assert airly.requests_per_day == 1000
+            assert airly.requests_remaining == 993
+
+    async def test_invalid_headers(self):
+        with open("data/measurements_typical.json") as file:
+            data = json.load(file)
+        headers = {}
+        with Mock(
+            get=AsyncMock(side_effect=data),
+            headers=Mock(side_effect=headers),
+            status=200,
+            __enter__=Mock(),
+            __exit__=Mock(),
+        ) as mock_session:
+            airly = Airly("abcdef", mock_session)
+            measurements = airly.create_measurements_session_point(LATITUDE, LONGITUDE)
+            await measurements.update()
+            assert airly.requests_per_day == None
+            assert airly.requests_remaining == None
